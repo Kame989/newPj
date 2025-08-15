d5cf1851538869afaabc8c523ffb35acf7fdc4ae
Kame989 <arpawee31keng@gmail.com>
2025-08-15 21:41:16 +0700

    try

--- dcode.py
+++ dcode.py
@@ -1,5 +1,32 @@
-a = input()
-n = int(input())
-m_list= n.split()
-print(m_list)
-print(a)
+# read the string
+s = input().strip()            # example: myoxjmvle
+
+# read the line with numbers (like: [8, 3, 7, 9]  or  8 3 7 9  or 8,3,7,9)
+line = input().strip()
+
+# remove brackets and commas, then split into pieces
+line = line.replace('[', '').replace(']', '').replace(',', ' ')
+parts = line.split()          # now parts is like ['8', '3', '7', '9']
+
+print(type(parts))
+print(line)
+# convert parts to integers
+
+nums = []
+for p in line:
+    nums.append(int(p))
+
+print(nums)
+
+aws  = ' '
+for n in nums:
+    if 1 <= n <= len(s):
+        aws = aws + s[n-1]
+
+print(aws)
+
+'''
+Humans usually say the 1st letter, 2nd letter, … — that’s 1-based counting.
+Python uses 0-based indexes, so the first letter is s[0], the second is s[1], etc.
+
+If you want the n-th letter from human counting, you must use s[n-1] in Python (because Python starts at 0).'''
