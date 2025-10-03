e880d9fb9c002a0970ee0915909cb045b55f7168
Kame989 <arpawee31keng@gmail.com>
2025-10-03 23:12:24 +0700

    fhjfjbm

---
+++ funnn.py
@@ -0,0 +1,13 @@
+#User function Template for python3
+
+#Write the complete argumentFunction below.
+#The function should take two arguments a and b
+#The function should return a+b
+#code here
+
+#Write the argumentFunction above.
+def  argumentFunction(a,b):
+    return a+b
+
+print(argumentFunction(2,3))
+
