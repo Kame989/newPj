bb3f98ae3eeec9847d3aa6bba3dd339ee8101f66
Kame989 <arpawee31keng@gmail.com>
2025-10-17 23:48:06 +0700

    hgkkugug

---
+++ pracfun.py
@@ -0,0 +1,35 @@
+def my_function():
+    print("hello w")
+    print("h p")
+
+my_function()
+
+def my_f():
+    print("hp"*2, end = '\n')
+
+my_f()
+
+def fah_to_cel(fah):
+    return (fah - 32) *5/9
+
+print(fah_to_cel(100)) 
+print(fah_to_cel(50))
+print(fah_to_cel(32))
+
+def grt(x):
+    return "Good morning " + x
+
+msg = grt("John")
+print(msg)
+
+def mul (x):
+    return x*5
+
+x = int(input())
+print(mul(x))
+
+def prime(p):
+    
+
+
+
